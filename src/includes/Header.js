81a55ece24f8070391edb63e88d9012d488a453d
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2024-02-25 14:10:57 +0530

    commit files"

--- src/includes/Header.js
+++ src/includes/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 // import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import Logo from "../assets/img/logo-color.svg";
 
 function Header() {
+  
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -27,50 +28,96 @@ function Header() {
     <div>
       <header className={isScrolled ? "fixed-top-nav fixed-top" : "fixed-top"}>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light shadow">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-              <NavLink className="navbar-brand" to="/">
-                <img src={Logo} alt="" />
-              </NavLink>
               <button
                 className="navbar-toggler"
                 type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar"
               >
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav ms-auto">
-                  <li className="nav-item">
-                    <NavLink className="nav-link" aria-current="page" to="/">
-                      Home
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/About">
-                      About
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/Products">
-                      Products
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/Services">
-                      Services
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/Contact">
-                      Contact
-                    </NavLink>
-                  </li>
-                </ul>
+              <a className="navbar-brand" href="#">
+                Navbar
+              </a>
+              <div
+                className="offcanvas offcanvas-start"
+                tabIndex="-1"
+                id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel"
+              >
+                <div className="offcanvas-header">
+                  <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                    Offcanvas
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn-close text-reset"
+                    data-bs-dismiss="offcanvas"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="offcanvas-body">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active"
+                        aria-current="page"
+                        href="#"
+                      >
+                        Home
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        Link
+                      </a>
+                    </li>
+                    <li className="nav-item dropdown">
+                      <a
+                        className="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Dropdown
+                      </a>
+                      <ul
+                        className="dropdown-menu"
+                        aria-labelledby="navbarDropdown"
+                      >
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Action
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Another action
+                          </a>
+                        </li>
+                        <li>
+                          <hr className="dropdown-divider" />
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Something else here
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        About
+                      </a>
+                    </li>
+                    {/* Add more menu items as needed */}
+                  </ul>
+                </div>
               </div>
             </div>
           </nav>
