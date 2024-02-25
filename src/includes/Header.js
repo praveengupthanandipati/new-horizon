d1b112939f3991ccb8b1be5c87da8a9b77ae3ef3
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2024-02-25 19:58:33 +0530

    commit files

--- src/includes/Header.js
+++ src/includes/Header.js
@@ -5,7 +5,6 @@ import { NavLink } from "react-router-dom";
 import Logo from "../assets/img/logo-color.svg";
 
 function Header() {
-  
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -28,10 +27,10 @@ function Header() {
     <div>
       <header className={isScrolled ? "fixed-top-nav fixed-top" : "fixed-top"}>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
+          <nav className="navbar navbar-expand-lg navbar-light shadow px-4">
+            <div className="navbarnav flex-nowrap">
               <button
-                className="navbar-toggler"
+                className="navbar-toggler order-last"
                 type="button"
                 data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasNavbar"
@@ -39,44 +38,41 @@ function Header() {
               >
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <a className="navbar-brand" href="#">
-                Navbar
-              </a>
-              <div
-                className="offcanvas offcanvas-start"
-                tabIndex="-1"
-                id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel"
-              >
-                <div className="offcanvas-header">
-                  <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                    Offcanvas
-                  </h5>
-                  <button
-                    type="button"
-                    className="btn-close text-reset"
-                    data-bs-dismiss="offcanvas"
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="offcanvas-body">
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                      <a
-                        className="nav-link active"
-                        aria-current="page"
-                        href="#"
-                      >
-                        Home
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">
-                        Link
-                      </a>
-                    </li>
-                    <li className="nav-item dropdown">
-                      <a
+              <NavLink className="navbar-brand" href="#">
+                <img src={Logo} alt="" />
+              </NavLink>
+            </div>
+            <div
+              className="offcanvas offcanvas-end"
+              tabIndex="-1"
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                  <img src={Logo} alt="" />
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close text-reset"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav ms-auto">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" aria-current="page" to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/About">
+                      About
+                    </NavLink>
+                  </li>
+                  {/* <li className="nav-item dropdown">
+                      <NavLink
                         className="nav-link dropdown-toggle"
                         href="#"
                         id="navbarDropdown"
@@ -85,7 +81,7 @@ function Header() {
                         aria-expanded="false"
                       >
                         Dropdown
-                      </a>
+                      </NavLink>
                       <ul
                         className="dropdown-menu"
                         aria-labelledby="navbarDropdown"
@@ -99,25 +95,30 @@ function Header() {
                           <a className="dropdown-item" href="#">
                             Another action
                           </a>
-                        </li>
-                        <li>
-                          <hr className="dropdown-divider" />
-                        </li>
+                        </li>                        
                         <li>
                           <a className="dropdown-item" href="#">
                             Something else here
                           </a>
                         </li>
                       </ul>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">
-                        About
-                      </a>
-                    </li>
-                    {/* Add more menu items as needed */}
-                  </ul>
-                </div>
+                    </li> */}
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/Products">
+                      Products
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/Services">
+                      Services
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/Contact">
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
               </div>
             </div>
           </nav>
